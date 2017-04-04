65f5b21b2f82ff63b7824eadcd68c8db37186255
Kiriphorito <sam.h.pham@gmail.com>
2017-04-04 15:34:22 +0100

    Added pem and password to deploy to server. Also auto install apache if not installed

--- MainApp/LSCK/LSCK/SSH.cs
+++ MainApp/LSCK/LSCK/SSH.cs
@@ -1,42 +1,63 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Renci.SshNet;
+using System.Windows.Forms;
 
 namespace LSCK
 {
     public class SSH
     {
-        string generateDir;
+        private string generateDir;
 
         public SSH(string generateDir)
         {
             this.generateDir = generateDir;
         }
 
-        public ConnectionInfo CreateConnectionInfo(string ip, string username, string privateKeyFilePath)
+        public ConnectionInfo CreateConnectionInfo(string ip, string username, string type, string details)
         {
-            ConnectionInfo connectionInfo;
-            using (var stream = new FileStream(privateKeyFilePath, FileMode.Open, FileAccess.Read))
+            AuthenticationMethod authenticationMethod = null;
+            switch (type)
             {
-                var privateKeyFile = new PrivateKeyFile(stream);
-                AuthenticationMethod authenticationMethod = new PrivateKeyAuthenticationMethod(username, privateKeyFile);
-                connectionInfo = new ConnectionInfo(ip, username, authenticationMethod);
+                case "pem":
+                    string privateKeyFilePath = details;
+                    using (var stream = new FileStream(privateKeyFilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        var privateKeyFile = new PrivateKeyFile(stream);
+                        authenticationMethod = new PrivateKeyAuthenticationMethod(username, privateKeyFile);
+                    }
+                    break;
+                case "password":
+                    authenticationMethod = new PasswordAuthenticationMethod(username, details);
+                    break;
+                default:
+                    MessageBox.Show("Invalid authentication method");
+                    break;
             }
+            ConnectionInfo connectionInfo = new ConnectionInfo(ip, username, authenticationMethod);
             return connectionInfo;
         }
 
 
-        public void UploadWebsite()
+        public void UploadWebsite(string ip, string username, string type, string details)
         {
-            using (var ssh = new SshClient(CreateConnectionInfo("52.56.181.183", "ubuntu", @"C:\Users\Sam\Downloads\comp205p.pem")))
+            using (var ssh = new SshClient(CreateConnectionInfo(ip, username, type, details)))
             {
                 ssh.Connect();
-                var command = ssh.CreateCommand("rm -r *");
-                var result = command.Execute();
-                Console.Out.WriteLine(result);
+                string result = ssh.CreateCommand("apache2 -v").Execute();
+                if (!result.Contains("version"))
+                {
+                    MessageBox.Show("Installing Apache");
+                    ssh.CreateCommand("sudo apt-get -y install apache2").Execute();
+                    ssh.CreateCommand("sudo apt-get -y install php libapache2-mod-php").Execute();
+                    ssh.CreateCommand("sudo /etc/init.d/apache2 restart").Execute();
+                    ssh.CreateCommand("sudo chown " + username + " /var/www/html/").Execute();
+                    MessageBox.Show("Installed Apache");
+                }
+                //else { MessageBox.Show("Apache Already Installed"); }
+                ssh.CreateCommand("rm -r /var/www/html/*").Execute();
                 ssh.Disconnect();
             }
-            using (var scp = new ScpClient(CreateConnectionInfo("52.56.181.183", "ubuntu", @"C:\Users\Sam\Downloads\comp205p.pem")))
+            using (var scp = new ScpClient(CreateConnectionInfo(ip, username, type, details)))
             {
                 scp.Connect();
                 scp.Upload(new DirectoryInfo(generateDir), "/var/www/html/");
